531ff65fc05a1619460ec2f13e29f06386a10ac5
Nahishu <NahishuHailar@gmail.com>
2024-04-26 17:38:44 +0300

    Added FireBase user authentications

--- habbit/client/views.py
+++ habbit/client/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import permissions
 from django.shortcuts import get_object_or_404
 
 from users.models import User
@@ -7,28 +8,41 @@ from .serializers import HabbitGroupSerializer, UserSerializer, HabbitSerializer
 
 
 class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    """Read, update, delete user profile"""
+    """
+    Read, update, delete user profile
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
 
 class HabbitListCreateApiView(generics.ListCreateAPIView):
-    """Get list of habits of current user, create new habit"""
-    serializer_class = HabbitSerializer    
+    """
+    Get list of habits of current user, create new habit
+    """
+    serializer_class = HabbitSerializer
+    permission_classes = (permissions.IsAuthenticated, )    
 
     def get_queryset(self):
         query_set = Habbit.objects.filter(user=self.kwargs['pk'])
         return query_set
 
 
+
 class HabbitRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    """Read, update, delete current users habit"""
+    """
+    Read, update, delete current users habit
+    """
     serializer_class = HabbitSerializer
     queryset = Habbit.objects.all()
+    permission_classes = (permissions.IsAuthenticated, )
 
 class Habbitdates(generics.ListAPIView):
-    """Read all users habit progress updates"""
+    """
+    Read all users habit progress updates
+    """
     serializer_class = HabbitSerializerdates
+    permission_classes = (permissions.IsAuthenticated, )
 
     def get_queryset(self):
         query_set = HabbitProgress.objects.values('update_time').filter(user=self.kwargs['pk'])
@@ -36,8 +50,11 @@ class Habbitdates(generics.ListAPIView):
     
 
 class CurrentHabbitdates(generics.ListAPIView):
-    """Read current users habit progress updates"""
+    """
+    Read current users habit progress updates
+    """
     serializer_class = HabbitSerializerdates
+    permission_classes = (permissions.IsAuthenticated, )
 
     def get_queryset(self):
         query_set = HabbitProgress.objects.values('update_time').filter(user=self.kwargs['pk']).filter(habbit=self.kwargs['habbit_id'])
@@ -45,6 +62,9 @@ class CurrentHabbitdates(generics.ListAPIView):
     
 
 class HabbitGroupListApiView(generics.ListAPIView):
-    """Read all habit groups"""
+    """
+    Read all habit groups
+    """
     queryset = HabbitGroup.objects.all()
     serializer_class = HabbitGroupSerializer
+    permission_classes = (permissions.IsAuthenticated, )
--- habbit/habbit/settings.py
+++ habbit/habbit/settings.py
@@ -128,3 +128,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'REST_framework.authentication.SessionAuthentication',
+        'firebase_auth.authentication.FirebaseAuthentication',
+    ),
+}
