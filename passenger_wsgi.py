490127244d8e70bef18133175adb92e0364faa65
Nahishu <nahishuhailar@gmail.com>
2024-05-04 11:46:16 +0300

    r

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
- 
-import os, sys
-sys.path.insert(0, '/root/projects/glossy-habit-tracker-backend')
-sys.path.insert(1, '/root/projects/glossy-habit-tracker-backend/venv/lib/python3.10/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'habbit.settings'
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
