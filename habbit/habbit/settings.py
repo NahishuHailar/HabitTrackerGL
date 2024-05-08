5ff0a794777e74d3fe918c1d4af4fe23eabbce75
Nahishu <nahishuhailar@gmail.com>
2024-05-08 23:22:24 +0300

    changed db settings - add psql

--- habbit/habbit/settings.py
+++ habbit/habbit/settings.py
@@ -78,13 +78,25 @@ WSGI_APPLICATION = 'habbit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASES = {
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'habit',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '4721',
+
+        'HOST': 'localhost',
+
+        'PORT': 5432,
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
