16059b532ac860f70783ad5fe1c7e935eef80115
Nahishu <nahishuhailar@gmail.com>
2024-05-20 11:27:12 +0300

    Added settings for firebase

--- habbit/habbit/settings.py
+++ habbit/habbit/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(##jo*(a%ir1!9q1mvz0mrsb^ij=uw@autsg)#qx(%-zr_kpju'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -138,7 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- habbit/users/exceptions.py
+++ habbit/users/exceptions.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework import APIException
+from rest_framework.exceptions import APIException
 
 
 class NoAuthToken(APIException):
