9b2d4847d16f02ebbc215dbaadb9fa7215ad71e5
Nahishu <nahishuhailar@gmail.com>
2024-08-05 15:21:50 +0300

    Stable version 1

---
+++ habit/manage_hab/constants.py
@@ -0,0 +1,26 @@
+PROGRESSSTATUS = (
+    ("active", "active"),
+    ("archive", "archive"),
+    ("done", "done"),
+    ("deleted", "deleted"),
+    ("none", "none"),
+)
+REPEATPERIOD = (
+    ("day", "day"),
+    ("week", "week"),
+    ("month", "month"),
+    ("year", "year"),
+    ("always", "always"),
+)
+COLOR = (
+    ("yellow", "yellow"),
+    ("green", "green"),
+    ("blue", "blue"),
+    ("purple", "purple"),
+)
+TRACKTIME = (
+    ("all_day", "all_day"),
+    ("morning", "morning"),
+    ("afternoon", "afternoon"),
+    ("evening", "evening"),
+)
---
+++ habit/users/constants.py
@@ -0,0 +1,10 @@
+AUTHTYPE = (
+        ("google", "google"),
+        ("apple", "apple"),
+        ("none", "none"),
+    )
+GENDER = (
+        ("male", "male"),
+        ("female", "female"),
+        ("none", "none"),
+    )
