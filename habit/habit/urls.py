6a06994c7f3532bd99c35a156babf8950a86e234
Nahishu <nahishuhailar@gmail.com>
2024-08-07 18:19:49 +0300

    Sentry settings

--- habit/habit/urls.py
+++ habit/habit/urls.py
@@ -25,15 +25,4 @@ urlpatterns = [
 ]
 
 urlpatterns += swagger_urls
-
-# urls.py
-from django.urls import path
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-urlpatterns += [
-    path('sentry-debug/', trigger_error),
-    # ...
-]
-                  
+            
