c1c08080be31913c7ec5defa206b680395910391
Nahishu <nahishuhailar@gmail.com>
2024-08-16 12:05:08 +0300

    Fixed habit progress ( dateutil for different date format)

--- habit/manage_hab/services/due_dates.py
+++ habit/manage_hab/services/due_dates.py
@@ -1,21 +1,21 @@
 """Due_dates for all habit types(Habit.repeat_period)"""
 
-from datetime import datetime
+from dateutil import parser
+
 
 def get_numbers_of_due_dates(due_dates, period):
     """
-    Returns the ordinal number of the deadline days 
+    Returns the ordinal number of the deadline days
     depending on the period of repetition of the habit
     """
     result = []
     for date in due_dates:
-        date_obj = datetime.fromisoformat(date)
-        if period == 'week':
-            result.append(date_obj.weekday())  
-        elif period == 'month':
-            result.append(date_obj.day - 1)       
-        elif period == 'year':
-            result.append(date_obj.timetuple().tm_yday - 1)  
+        date_obj = parser.parse(date)
+        if period == "week":
+            result.append(date_obj.weekday())
+        elif period == "month":
+            result.append(date_obj.day - 1)
+        elif period == "year":
+            result.append(date_obj.timetuple().tm_yday - 1)
     result.sort()
     return tuple(result)
-
