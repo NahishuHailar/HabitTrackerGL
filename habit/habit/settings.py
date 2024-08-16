bf64e036e741d6d4e20fbe57942b7367a218619a
Nahishu <nahishuhailar@gmail.com>
2024-08-16 13:30:54 +0300

    test

--- habit/habit/settings.py
+++ habit/habit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-(##jo*(a%ir1!9q1mvz0mrsb^ij=uw@autsg)#qx(%-zr_kpju
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1",  os.environ.get("ALLOWED_HOST")]
+ALLOWED_HOSTS = ["127.0.0.1",  os.environ.get("ALLOWED_HOST"), 'dev.api.glossy.sealclass.com']
 
 
 # Application definition
