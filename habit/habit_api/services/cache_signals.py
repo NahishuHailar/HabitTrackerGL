2602a7e448428b9bae105848eea5c0bf726bf23f
Nahishu <nahishuhailar@gmail.com>
2024-08-25 10:55:42 +0300

    Fixed clearring cash for HabitGroup, Icon, UserAvatar by signals

--- habit/habit_api/services/cache_signals.py
+++ habit/habit_api/services/cache_signals.py
@@ -1,6 +1,7 @@
 """
 Deleting the cache when updating related models
 """
+
 from django.core.cache import cache
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
@@ -22,14 +23,16 @@ def clear_view_cache(view_name, args=None):
 @receiver(post_save, sender=HabitGroup)
 @receiver(post_delete, sender=HabitGroup)
 def clear_habit_group_cache(sender, **kwargs):
-    clear_view_cache('habit_groups')
+    clear_view_cache("habit_groups")
+
 
 @receiver(post_save, sender=Icon)
 @receiver(post_delete, sender=Icon)
 def clear_icon_cache(sender, **kwargs):
-    clear_view_cache('icon_list')
+    clear_view_cache("icon_list")
+
 
 @receiver(post_save, sender=UserAvatar)
 @receiver(post_delete, sender=UserAvatar)
 def clear_avatar_cache(sender, **kwargs):
-    clear_view_cache('avatar_list')
+    clear_view_cache("avatar_list")
--- habit/manage_hab/apps.py
+++ habit/manage_hab/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ManageHabConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "manage_hab"
+
+    def ready(self):
+        import habit_api.services.cache_signals
+
