0902cb644739d05377571ecc0d535643818e5eb4
Nahishu <nahishuhailar@gmail.com>
2024-09-21 20:47:03 +0300

    New project architecture

--- habit/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path, include
-
-
-urlpatterns = [
-    path("/drf-auth/", include("rest_framework.urls")),
-    path("djoser/auth/", include("djoser.urls")),
-    re_path(r"^auth/", include("djoser.urls.authtoken")),
-]
--- habit/users/utils.py
+++
@@ -1,6 +0,0 @@
-def user_directory_path(instance, filename="profile"):
-    """
-    Upload user profile photo
-    file will be uploaded to MEDIA_ROOT/users/user_<id>/<filename>
-    """
-    return f"users/{instance.id}/profile_photo/{filename}"
